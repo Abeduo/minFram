91e4f683f7a7835abe75f48d8892f451bdaaffaf
Abeduo <2941738221@qq.com>
2025-05-18 16:28:56 +0800

    Add files via upload

---
+++ src/test/java/biao42.java
@@ -0,0 +1,17 @@
+import CORE.MultiGraphArrangement;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class biao42 {
+    public static void main(String[] args) throws IOException {
+        List<String> filePaths = new ArrayList<>();
+        filePaths.add("src/main/resources/pic/测试相交1.png");
+        filePaths.add("src/main/resources/pic/测试相交1.png");
+        MultiGraphArrangement.core(filePaths);
+    }
+}
